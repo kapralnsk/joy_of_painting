6a9c5d45de743f2a1ee468ca589de4de7a3a3a84
Aleksandr Kaurdakov <kapralnsk@gmail.com>
2017-01-26 23:05:19 +0700

    move db host and name to env variable

--- src/db.py
+++ src/db.py
@@ -2,6 +2,7 @@ import motor.motor_asyncio
 from os import environ
 
 db_name = environ['DB_NAME']
+db_host = environ['DB_HOST']
 
-client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://db:27017')
+client = motor.motor_asyncio.AsyncIOMotorClient(f'mongodb://{db_host}:27017')
 db = client[db_name]
