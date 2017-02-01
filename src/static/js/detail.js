8de295b25053ffa4272f00806942b587a7116d0f
Aleksandr Kaurdakov <kapralnsk@gmail.com>
2017-02-01 12:44:57 +0700

    fixes #1

--- src/static/js/detail.js
+++ src/static/js/detail.js
@@ -2,10 +2,6 @@ $('#delete').click(function(){
     $('#delete-modal').modal('show');
 })
 
-$('#edit').click(function(){
-    window.location.href
-})
-
 $('#delete-confirm').click(function(event){
     $.ajax({
         url: '/image/' + window.location.pathname.split('/').slice(-1)[0],
